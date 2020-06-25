fb9453b028b630ca44f9516e1304934fb9fefdfd
Vince Suriani <chenzorama@hotmail.com>
2020-06-25 14:55:52 -0400

    Moving along

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('a test Recipe', "This is a test only", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
+  ];
+
 
   constructor() { }
 
